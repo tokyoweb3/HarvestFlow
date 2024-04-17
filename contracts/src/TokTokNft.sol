f3f8d93375c61ee0292b84babeba9bd2dd009c75
Matej Poklemba <matejpoklemba@gmail.com>
2024-04-17 16:09:50 +0200

    Prepare basics

---
+++ contracts/src/TokTokNft.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.20;
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+
+contract TokTokNft is ERC1155 {
+    constructor(string memory uri_) ERC1155(uri_) {}
+}
