c4b5a29016aed904a1e0311911a8de4b201ad86e
Dávid Novák <david.novak@vacuumlabs.com>
2024-05-24 10:58:54 +0200

    Add tailwind

--- harvest-flow/frontend/tools/webpack/webpack.rules.js
+++ harvest-flow/frontend/tools/webpack/webpack.rules.js
@@ -1,5 +1,5 @@
-const { inDev } = require('./webpack.helpers');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { inDev } = require("./webpack.helpers");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = [
   {
@@ -7,45 +7,50 @@ module.exports = [
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
     use: {
-      loader: 'ts-loader',
+      loader: "ts-loader",
       options: {
         transpileOnly: true,
       },
     },
   },
+  // {
+  //   // CSS Loader
+  //   test: /\.css$/,
+  //   use: [
+  //     { loader: inDev() ? "style-loader" : MiniCssExtractPlugin.loader },
+  //     { loader: "css-loader" },
+  //   ],
+  // },
   {
-    // CSS Loader
-    test: /\.css$/,
-    use: [
-      { loader: inDev() ? 'style-loader' : MiniCssExtractPlugin.loader },
-      { loader: 'css-loader' },
-    ],
+    test: /\.css$/i,
+    // include: path.resolve(__dirname, "src"),
+    use: ["style-loader", "css-loader", "postcss-loader"],
   },
-  {
-    // SCSS (SASS) Loader
-    test: /\.s[ac]ss$/i,
-    use: [
-      { loader: inDev() ? 'style-loader' : MiniCssExtractPlugin.loader },
-      { loader: 'css-loader' },
-      { loader: 'sass-loader' },
-    ],
-  },
+  // {
+  //   // SCSS (SASS) Loader
+  //   test: /\.css$/i,
+  //   use: [
+  //     { loader: inDev() ? "style-loader" : MiniCssExtractPlugin.loader },
+  //     { loader: "css-loader" },
+  //     { loader: "sass-loader" },
+  //   ],
+  // },
   {
     // Less loader
     test: /\.less$/,
     use: [
-      { loader: inDev() ? 'style-loader' : MiniCssExtractPlugin.loader },
-      { loader: 'css-loader' },
-      { loader: 'less-loader' },
+      { loader: inDev() ? "style-loader" : MiniCssExtractPlugin.loader },
+      { loader: "css-loader" },
+      { loader: "less-loader" },
     ],
   },
   {
     // Assets loader
     // More information here https://webpack.js.org/guides/asset-modules/
     test: /\.(gif|jpe?g|tiff|png|webp|bmp|svg|eot|ttf|woff|woff2)$/i,
-    type: 'asset',
+    type: "asset",
     generator: {
-      filename: 'assets/[hash][ext][query]',
+      filename: "assets/[hash][ext][query]",
     },
   },
 ];
