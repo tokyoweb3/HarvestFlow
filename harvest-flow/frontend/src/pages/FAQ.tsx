56664e501b45d715ce599a98e42883f295834f72
Miklós Nagy <miklos.nagy@vacuumlabs.com>
2024-05-30 19:10:13 +0200

    Add ProjectHero address to FAQ page

--- harvest-flow/frontend/src/pages/FAQ.tsx
+++ harvest-flow/frontend/src/pages/FAQ.tsx
@@ -5,11 +5,15 @@ import ProjectHero from "@src/components/ProjectHero";
 import ProjectTabsSection from "@src/components/ProjectTabsSection";
 import FAQPageFAQSection from "@src/components/FAQPageFAQSection";
 import ProjectLendAHandSection from "@src/components/ProjectLendAHandSection";
+import { useSearchParams } from "react-router-dom";
 
 const FAQ: React.FC = () => {
+  const [searchParams] = useSearchParams();
+  const contractAddress = searchParams.get('address') || '';
+
   return (
     <Layout>
-      <ProjectHero />
+      <ProjectHero projectContractAddress={contractAddress} />
       <ProjectTabsSection activePage="qa" />
       <FAQPageFAQSection />
       <ProjectLendAHandSection />
