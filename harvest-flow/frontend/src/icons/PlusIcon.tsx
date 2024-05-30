2e070b1b0a3a8858bebae152e09d343df63bfc82
Dávid Novák <david.novak@vacuumlabs.com>
2024-05-30 09:40:30 +0200

    Add FAQ page layout

---
+++ harvest-flow/frontend/src/icons/PlusIcon.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PlusIcon: React.FC = () => (
+  <svg
+    width="15"
+    height="15"
+    viewBox="0 0 15 15"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 7.5H15" stroke="#282828" />
+    <path d="M7.5 0L7.5 15" stroke="#282828" />
+  </svg>
+);
+
+export default PlusIcon;
