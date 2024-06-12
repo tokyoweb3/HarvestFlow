373cf5f6fae87f84231330e4e020505ea5b89041
Dávid Novák <david.novak@vacuumlabs.com>
2024-06-12 11:24:48 +0200

    Account page mobile layout

--- harvest-flow/frontend/src/components/AccountProjectHistorySection.tsx
+++ harvest-flow/frontend/src/components/AccountProjectHistorySection.tsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect } from "react";
-import MainController from "@src/MainController";
+import type MainController from "@src/MainController";
 import { AppContext } from "@src/main";
-import { NftHistoryEvent } from "@harvest-flow/utils";
-import { ethers } from "ethers";
+import type { NftHistoryEvent } from "@harvest-flow/utils";
 import { CHAIN_EXPLORER_URI } from "@src/utils/constants";
 import { formatTimestampForHistoryTable, middleEllipsis } from "@src/utils";
+import { ethers } from "ethers/lib";
 
 const AccountProjectHistorySection: React.FC = () => {
   const mainController: MainController = useContext(AppContext);
@@ -12,59 +12,69 @@ const AccountProjectHistorySection: React.FC = () => {
   const [nftHistory, setNftHistory] = React.useState<NftHistoryEvent[]>([]);
 
   useEffect(() => {
-      mainController.getNftHistoryForUser().then((response) => {
-        if (response.history) {
-          setNftHistory(response.history);
-        }
-      });
-  },[mainController]);
+    mainController.getNftHistoryForUser().then((response) => {
+      if (response.history) {
+        setNftHistory(response.history);
+      }
+    });
+  }, [mainController]);
   return (
     <div className="flex flex-col gap-14">
-      <h2 className="text-center text-heading3 font-medium uppercase">
+      <h2 className="text-center text-heading4 desktop:text-heading3 font-medium uppercase">
         Project History
       </h2>
       <div className="w-full border border-black divide-x divide-black">
         <table className="w-full">
           <thead>
             <tr className="border-b border-black border-dashed">
-              <th className="uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
+              <th className="text-caption desktop:text-body uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
                 Type
               </th>
-              <th className="uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
+              <th className="text-caption desktop:text-body uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
                 Price
               </th>
-              <th className="uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
+              <th className="text-caption desktop:text-body uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
                 Project
               </th>
-              <th className="uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
+              <th className="text-caption desktop:text-body uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
                 Tx
               </th>
-              <th className="uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
+              <th className="text-caption desktop:text-body uppercase text-center font-normal p-4 border-r border-black last-of-type:border-0 w-1/4">
                 Time
               </th>
             </tr>
           </thead>
           <tbody>
-          {nftHistory.sort((a, b) => b.timestamp - a.timestamp)
-            .map((row, index) => (
-              <tr className="border-b border-black border-dashed last-of-type:border-0">
-                <td className="text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 bg-secondary text-white w-1/4">
-                  {row.eventType}
-                </td>
-                <td className="text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
-                  {ethers.utils.formatEther(row.price)} DAI
-                </td>
-                <td className="text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
-                  {row.projectName}
-                </td>
-                <td className="text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
-                  <a target="_blank" href={`${CHAIN_EXPLORER_URI}/tx/${row.transactionHash}`}>{middleEllipsis(row.transactionHash)}</a>
-                </td>
-                <td className="text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
-                  {formatTimestampForHistoryTable(row.timestamp)}
-                </td>
-              </tr>
-            ))}
+            {nftHistory
+              .sort((a, b) => b.timestamp - a.timestamp)
+              .map((row, index) => (
+                <tr
+                  className="border-b border-black border-dashed last-of-type:border-0"
+                  key={index}
+                >
+                  <td className="text-body desktop:text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 bg-secondary text-white w-1/4">
+                    {row.eventType}
+                  </td>
+                  <td className="text-body desktop:text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
+                    {ethers.utils.formatEther(row.price)} DAI
+                  </td>
+                  <td className="text-body desktop:text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
+                    {row.projectName}
+                  </td>
+                  <td className="text-body desktop:text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href={`${CHAIN_EXPLORER_URI}/tx/${row.transactionHash}`}
+                    >
+                      {middleEllipsis(row.transactionHash)}
+                    </a>
+                  </td>
+                  <td className="text-body desktop:text-heading5 uppercase text-center p-8 border-r border-black last-of-type:border-0 w-1/4">
+                    {formatTimestampForHistoryTable(row.timestamp)}
+                  </td>
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
--- harvest-flow/frontend/src/components/AccountYourNFTSection.tsx
+++ harvest-flow/frontend/src/components/AccountYourNFTSection.tsx
@@ -11,8 +11,7 @@ import NFTProjectCard, { NFTProjectComingSoonCard } from "./NFTProjectCard";
 import tukTukImage from "../../assets/images/tuktuk.jpg";
 import type { NftDetails } from "@harvest-flow/utils";
 import { groupBy } from "@src/utils";
-import { ethers } from "ethers";
-
+import { ethers } from "ethers/lib";
 
 const nftItemData: NFTItemCardProps[] = [
   {
@@ -51,93 +50,117 @@ const NFTList: React.FC<NFTListProps> = ({ projectName, nfts }) => {
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex items-center gap-2"
-           onClick={toggleCollapse}>
-        <h3 className="text-heading4 uppercase font-medium">
-          {projectName}
-        </h3>
+      <div className="flex items-center gap-2" onClick={toggleCollapse}>
+        <h3 className="text-heading4 uppercase font-medium">{projectName}</h3>
         {/* TODO add arrowup icon */}
-        {isCollapsed ? (<ArrowDownIcon />) : (<ArrowDownIcon />)}
+        {isCollapsed ? <ArrowDownIcon /> : <ArrowDownIcon />}
       </div>
       {!isCollapsed && (
-        <div className="grid grid-cols-3 gap-10">
-        {nfts.map((data) => (
-          <NFTCard
-            key={data.contractAddress.concat(data.tokenId.toString())}
-            contractAddress={data.contractAddress}
-            tokenId={data.tokenId}
-            asset={1} // TODO: What is this?
-            imageURL={data.imageURL}
-            investment={data.investment}
-            earned={data.earned}
-            termStart={data.termStart}
-            termEnd={data.termEnd}
-            apr={data.apr}
-           />
-        ))}
+        <div className="overflow-x-auto">
+          <div className="flex desktop:grid desktop:grid-cols-3 gap-6 desktop:gap-10">
+            {nfts.map((data) => (
+              <div
+                key={data.contractAddress.concat(data.tokenId.toString())}
+                className="w-[80vw] max-w-[300px] desktop:w-full desktop:max-w-[initial] shrink-0"
+              >
+                <NFTCard
+                  contractAddress={data.contractAddress}
+                  tokenId={data.tokenId}
+                  asset={1} // TODO: What is this?
+                  imageURL={data.imageURL}
+                  investment={data.investment}
+                  earned={data.earned}
+                  termStart={data.termStart}
+                  termEnd={data.termEnd}
+                  apr={data.apr}
+                />
+              </div>
+            ))}
+          </div>
         </div>
-       )}
+      )}
     </div>
   );
-}
+};
 
-const AccountYourNFTSection: React.FC<{ ownedNfts: NftDetails[] }> = ({ ownedNfts }) => {
-  const groupedNfts = groupBy(ownedNfts, nft => nft.contractAddress);
+const AccountYourNFTSection: React.FC<{ ownedNfts: NftDetails[] }> = ({
+  ownedNfts,
+}) => {
+  const groupedNfts = groupBy(ownedNfts, (nft) => nft.contractAddress);
 
   return (
     <div className="flex flex-col gap-14">
-      <h2 className="text-center text-heading3 font-medium uppercase">
+      <h2 className="text-center text-heading4 desktop:text-heading3 font-medium uppercase">
         Your NFT
       </h2>
-      <div className="w-full">
-        <div className="flex flex-col gap-20">
-          {
-            Object.keys(groupedNfts).map((contractAddress, index) => (
-              <NFTList key={index}
-                       projectName={groupedNfts[contractAddress][0].projectName}
-                       nfts={groupedNfts[contractAddress].map(nft => ({
-                         contractAddress: nft.contractAddress,
-                         tokenId: nft.tokenId,
-                         asset: 1, // TODO: What is this?
-                         imageURL: tukTukImage, // TODO: change this to the actual image
-                         investment: Number(ethers.utils.formatEther(nft.lendingData.principle)),
-                         earned: Number(ethers.utils.formatEther(nft.lendingData.claimedYield)),
-                         termStart: new Date(nft.lendingData.lendingStart),
-                         termEnd: new Date(nft.lendingData.lendingEnd),
-                         apr: Number(ethers.utils.formatEther(nft.lendingData.yield))*100,
-                       }))}
-              />
-            ))
-          }
-
-          <div className="flex flex-col gap-10">
-            <h3 className="text-heading4 uppercase font-medium">Items</h3>
-            <div className="grid grid-cols-3 gap-10">
-              {nftItemData.map((data, index) => (
-                <NFTItemCard
-                  key={index}
-                  imageURL={data.imageURL}
-                  text={data.text}
-                  caption={data.caption}
-                  title={data.title}
-                />
-              ))}
+      <div className="w-full px-4 desktop:px-0">
+        <div className="flex flex-col gap-14 desktop:gap-20">
+          {Object.keys(groupedNfts).map((contractAddress, index) => (
+            <NFTList
+              key={index}
+              projectName={groupedNfts[contractAddress][0].projectName}
+              nfts={groupedNfts[contractAddress].map((nft) => ({
+                contractAddress: nft.contractAddress,
+                tokenId: nft.tokenId,
+                asset: 1, // TODO: What is this?
+                imageURL: tukTukImage, // TODO: change this to the actual image
+                investment: Number(
+                  ethers.utils.formatEther(nft.lendingData.principle),
+                ),
+                earned: Number(
+                  ethers.utils.formatEther(nft.lendingData.claimedYield),
+                ),
+                termStart: new Date(nft.lendingData.lendingStart),
+                termEnd: new Date(nft.lendingData.lendingEnd),
+                apr:
+                  Number(ethers.utils.formatEther(nft.lendingData.yield)) * 100,
+              }))}
+            />
+          ))}
+          <div className="flex flex-col gap-6 desktop:gap-10">
+            <h3 className="text-heading5 desktop:text-heading4 uppercase font-medium">
+              Items
+            </h3>
+            <div className="overflow-x-auto">
+              <div className="flex desktop:grid desktop:grid-cols-3 gap-6 desktop:gap-10">
+                {nftItemData.map((data, index) => (
+                  <div
+                    key={index}
+                    className="w-[80vw] max-w-[300px] desktop:w-full desktop:max-w-[initial] shrink-0"
+                  >
+                    <NFTItemCard
+                      imageURL={data.imageURL}
+                      text={data.text}
+                      caption={data.caption}
+                      title={data.title}
+                    />
+                  </div>
+                ))}
+              </div>
             </div>
           </div>
-          <div className="flex flex-col gap-10">
-            <h3 className="text-heading4 uppercase font-medium">
+          <div className="flex flex-col gap-6 desktop:gap-10">
+            <h3 className="text-heading5 desktop:text-heading4 uppercase font-medium">
               Upcoming projects
             </h3>
-            <div className="grid grid-cols-3 gap-10">
-              {nftProjectsData.map((data, index) => (
-                <NFTProjectCard
-                  key={index}
-                  imageURL={data.imageURL}
-                  text={data.text}
-                  title={data.title}
-                />
-              ))}
-              <NFTProjectComingSoonCard />
+            <div className="overflow-x-auto">
+              <div className="flex desktop:grid desktop:grid-cols-3 gap-6 desktop:gap-10">
+                {nftProjectsData.map((data, index) => (
+                  <div
+                    key={index}
+                    className="w-[80vw] max-w-[300px] desktop:w-full desktop:max-w-[initial] shrink-0"
+                  >
+                    <NFTProjectCard
+                      imageURL={data.imageURL}
+                      text={data.text}
+                      title={data.title}
+                    />
+                  </div>
+                ))}
+                <div className="w-[80vw] max-w-[300px] desktop:w-full desktop:max-w-[initial] shrink-0 flex">
+                  <NFTProjectComingSoonCard />
+                </div>
+              </div>
             </div>
           </div>
         </div>
