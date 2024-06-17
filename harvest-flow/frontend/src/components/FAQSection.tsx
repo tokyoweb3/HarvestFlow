1a61e261df0772edd90ed2d9e67433715656d7d1
Dávid Novák <david.novak@vacuumlabs.com>
2024-06-17 10:43:37 +0200

    FAQ section motion animation

--- harvest-flow/frontend/src/components/FAQSection.tsx
+++ harvest-flow/frontend/src/components/FAQSection.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import type { ReactElement } from "react";
+import React, { useRef } from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 import SectionHeader from "./SectionHeader";
 import MinusIcon from "@src/icons/MinusIcon";
@@ -9,36 +12,135 @@ import bgVideo from "../../assets/videos/pc_color_high.mp4";
 const faqData = [
   {
     question:
-      "Dolor ullamco officia ad ex dolor consequat ex sunt excepteur id eu nulla nisi laboris.",
-    answer:
-      "Sit duis quis est officia nostrud magna. Duis sint reprehenderit eiusmod deserunt sit irure pariatur et. Ullamco laborum esse et sunt minim labore do laboris aliquip commodo cillum velit.",
+      "The vehicle status is displayed as NOT STARTED. When will it be updated?",
+    answer: (
+      <>
+        <p>
+          After the lending application period ends, the collected funds will be
+          transferred to the operating company, which will use them to purchase
+          the vehicle. Once the driver selection, device installation on the
+          vehicle, and release process are completed, the data will be linked
+          and the status will be updated
+        </p>
+      </>
+    ),
   },
   {
-    question: "Tempor cillum esse adipisicing sit sint consequat fugiat id.",
-    answer:
-      "Minim esse magna sunt Lorem ad incididunt esse. Id deserunt ut sint id occaecat duis cillum voluptate velit. Fugiat reprehenderit culpa est consectetur sit reprehenderit deserunt ex exercitation sunt est ullamco cupidatat laborum. In anim esse adipisicing cillum. Consequat enim aliqua aliquip nulla.",
+    question: "Where can I purchase DAI on the BASE chain?",
+    answer: (
+      <>
+        <p>
+          It can be exchanged for cryptocurrencies like ETH on platforms such as
+          Aerodome and Uniswap.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: "I do not own any cryptocurrencies. Can I still use the service?",
+    answer: (
+      <>
+        <p>
+          To use HARVEST FLOW, you must own cryptocurrencies in a supported
+          wallet. We provide a guide on how to purchase cryptocurrencies here.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: "Can the loan be canceled midway?",
+    answer: (
+      <>
+        <p>
+          The interest received at maturity is generally calculated using the
+          following formula:
+        </p>
+        <ul className="list-disc pl-4 pt-4">
+          <li>Principal: Lending amount (cryptocurrency)</li>
+          <li>
+            Interest: Lending amount (cryptocurrency) × Lending period × Annual
+            rate ÷ 365 days
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: "How can I get in touch for questions or inquiries?",
+    answer: (
+      <>
+        <p>All inquiries are accepted in the HARVEST HALL Discord.</p>
+      </>
+    ),
   },
   {
     question:
-      "Adipisicing commodo officia excepteur consequat in do irure laborum pariatur.",
-    answer:
-      "Et commodo est officia consequat officia pariatur veniam minim dolor consectetur ut veniam est. Deserunt eiusmod duis in duis dolor. Officia elit esse labore consectetur et reprehenderit. Commodo qui dolor eu id consectetur irure qui et tempor. Duis ex sint in proident ut laborum occaecat duis consectetur dolor. Aute deserunt sit mollit esse proident nulla est incididunt irure. Sint sint consequat qui consequat dolore in ullamco duis nostrud.",
+      "The vehicle status is displayed as NOT STARTED. When will it be updated?",
+    answer: (
+      <>
+        <p>
+          After the lending application period ends, the collected funds will be
+          transferred to the operating company, which will use them to purchase
+          the vehicle. Once the driver selection, device installation on the
+          vehicle, and release process are completed, the data will be linked
+          and the status will be updated
+        </p>
+      </>
+    ),
   },
   {
-    question: "Consectetur culpa id voluptate culpa irure mollit in.",
-    answer:
-      "Ex dolore id non tempor nisi labore laboris adipisicing consequat deserunt cupidatat voluptate esse. Sint deserunt anim cupidatat sunt labore est pariatur ad nostrud exercitation ea. In labore ea duis laborum minim deserunt ut ea commodo veniam.",
+    question: "Where can I purchase DAI on the BASE chain?",
+    answer: (
+      <>
+        <p>
+          It can be exchanged for cryptocurrencies like ETH on platforms such as
+          Aerodome and Uniswap.
+        </p>
+      </>
+    ),
   },
   {
-    question: "Non officia Lorem dolore nostrud do reprehenderit.",
-    answer:
-      "Voluptate amet incididunt ea consectetur ut reprehenderit sit culpa aliqua voluptate incididunt et irure. Occaecat sit do excepteur exercitation sit cupidatat aute anim voluptate consequat elit. Eu ipsum ullamco non qui ex. Officia mollit veniam commodo id eiusmod dolore laborum officia nostrud. Nostrud nisi magna sit exercitation deserunt aliquip deserunt exercitation elit nostrud do.",
+    question: "I do not own any cryptocurrencies. Can I still use the service?",
+    answer: (
+      <>
+        <p>
+          To use HARVEST FLOW, you must own cryptocurrencies in a supported
+          wallet. We provide a guide on how to purchase cryptocurrencies here.
+        </p>
+      </>
+    ),
+  },
+  {
+    question: "Can the loan be canceled midway?",
+    answer: (
+      <>
+        <p>
+          The interest received at maturity is generally calculated using the
+          following formula:
+        </p>
+        <ul className="list-disc pl-4 pt-4">
+          <li>Principal: Lending amount (cryptocurrency)</li>
+          <li>
+            Interest: Lending amount (cryptocurrency) × Lending period × Annual
+            rate ÷ 365 days
+          </li>
+        </ul>
+      </>
+    ),
+  },
+  {
+    question: "How can I get in touch for questions or inquiries?",
+    answer: (
+      <>
+        <p>All inquiries are accepted in the HARVEST HALL Discord.</p>
+      </>
+    ),
   },
 ];
 
 const FAQItem: React.FC<{
   question: string;
-  answer: string;
+  answer: string | ReactElement;
   index: number;
   openByDefault?: boolean;
 }> = ({ question, answer, index, openByDefault }) => {
@@ -46,8 +148,8 @@ const FAQItem: React.FC<{
 
   return (
     <div className="flex divide-x divide-black border-t border-black">
-      <div className="px-3 py-6 desktop:p-10">
-        <p className="text-body desktop:text-heading5">Q{index}</p>
+      <div className="px-3 py-6 desktop:p-10 w-16 desktop:w-28">
+        <p className="text-body desktop:text-heading5 text-center">Q{index}</p>
       </div>
       <div className="px-4 desktop:px-10 py-4 desktop:py-6 flex flex-col gap-4 flex-1">
         <div className="flex justify-between flex-1 items-start gap-10">
@@ -56,47 +158,94 @@ const FAQItem: React.FC<{
             {isOpen ? <MinusIcon /> : <PlusIcon />}
           </button>
         </div>
-        {isOpen && <p className="text-caption desktop:text-body">{answer}</p>}
+        {isOpen && (
+          <div className="text-caption desktop:text-body">{answer}</div>
+        )}
       </div>
     </div>
   );
 };
 
 const FAQSection: React.FC = () => {
+  const container = useRef();
+  const scrollableTextWrapper = useRef<HTMLDivElement>();
+
+  useGSAP(
+    () => {
+      if (!scrollableTextWrapper.current) return;
+
+      // disable on mobile
+      if (window.innerWidth < 1200) return;
+
+      // disable scroll trigger if content is less than window height
+      // if (scrollableTextWrapper.current.scrollHeight < window.innerHeight)
+      //   return;
+
+      gsap.to(".gsap-faq-container", {
+        scrollTrigger: {
+          trigger: ".gsap-faq-inner",
+          start: "top top",
+          end: `+=${scrollableTextWrapper.current.scrollHeight}px`,
+          pin: true,
+          markers: true,
+        },
+      });
+
+      gsap.to(".gsap-faq-text-scroll-container", {
+        scrollTo: scrollableTextWrapper.current.scrollHeight,
+        scrollTrigger: {
+          trigger: ".gsap-faq-inner",
+          start: "top top",
+          end: `+=${scrollableTextWrapper.current.scrollHeight + window.innerHeight - 160}px`,
+          markers: true,
+          scrub: true,
+        },
+      });
+    },
+    { scope: container },
+  );
+
   return (
-    <div className="flex flex-col desktop:flex-row desktop:divide-x desktop:divide-black desktop:border-b desktop:border-black relative z-10 desktop:h-screen bg-white">
-      <div className="w-full desktop:w-1/2 desktop:flex desktop:flex-col desktop:justify-between">
-        <SectionHeader title="FAQ" />
-        <div className="desktop:pt-24">
-          <div className="px-10 pb-10">
-            <h3 className="text-heading5 desktop:text-heading4 text-center desktop:text-left uppercase font-medium">
-              An investment experience that transforms society with emotion.
-            </h3>
+    <div ref={container}>
+      <div className="gsap-faq-container">
+        <div className="flex flex-col desktop:flex-row desktop:divide-x desktop:divide-black desktop:border-b desktop:border-black relative z-10 desktop:h-screen bg-white gsap-faq-inner">
+          <div className="w-full desktop:w-1/2 desktop:flex desktop:flex-col desktop:justify-between">
+            <SectionHeader title="FAQ" />
+            <div
+              className="desktop:pt-24 desktop:overflow-y-hidden gsap-faq-text-scroll-container"
+              ref={scrollableTextWrapper}
+            >
+              <div className="px-10 pb-10">
+                <h3 className="text-heading5 desktop:text-heading4 text-center desktop:text-left uppercase font-medium">
+                  An investment experience that transforms society with emotion.
+                </h3>
+              </div>
+              <div className="border-b border-black desktop:border-0">
+                {faqData.map((faqItem, index) => (
+                  <FAQItem
+                    key={index}
+                    index={index + 1}
+                    question={faqItem.question}
+                    answer={faqItem.answer}
+                  />
+                ))}
+              </div>
+            </div>
           </div>
-          <div className="border-b border-black desktop:border-0">
-            {faqData.map((faqItem, index) => (
-              <FAQItem
-                key={index}
-                index={index + 1}
-                question={faqItem.question}
-                answer={faqItem.answer}
-              />
-            ))}
+          <div className="hidden desktop:block w-1/2 bg-cover bg-no-repeat bg-center relative">
+            <video
+              src={bgVideo}
+              className="w-full h-full object-cover"
+              autoPlay
+              loop
+              muted
+            />
+            <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-full flex items-center justify-center z-10">
+              <h2 className="text-white text-heading3 desktop:text-heading2 font-medium uppercase tracking-widest text-center">
+                FAQ
+              </h2>
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="hidden desktop:block w-1/2 bg-cover bg-no-repeat bg-center relative">
-        <video
-          src={bgVideo}
-          className="w-full h-full object-cover"
-          autoPlay
-          loop
-          muted
-        />
-        <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-full flex items-center justify-center z-10">
-          <h2 className="text-white text-heading3 desktop:text-heading2 font-medium uppercase tracking-widest text-center">
-            FAQ
-          </h2>
         </div>
       </div>
     </div>
