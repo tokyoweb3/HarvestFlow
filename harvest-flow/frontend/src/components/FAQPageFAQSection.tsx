2ecbd2e1693881c141376d9379a55ea8e51d80b2
Dávid Novák <david.novak@vacuumlabs.com>
2024-07-25 11:20:34 +0200

    Mobile optimization for FAQ page

--- harvest-flow/frontend/src/components/FAQPageFAQSection.tsx
+++ harvest-flow/frontend/src/components/FAQPageFAQSection.tsx
@@ -13,22 +13,26 @@ const FAQItem: React.FC<{
   const [isOpen, setIsOpen] = React.useState(openByDefault);
 
   return (
-    <div className="border border-black px-10 py-[34px] bg-white">
-      <div className="flex gap-6 desktop:gap-10">
-        <div className="w-4 desktop:w-10">
+    <div className="border border-black px-[22px] py-[25px] desktop:px-10 desktop:py-[34px] bg-white">
+      <div className="flex gap-2 desktop:gap-10">
+        <div className="w-8 desktop:w-10">
           <p className="text-body desktop:text-heading5SmallerLH24">Q{index}</p>
         </div>
         <div
-          className="flex justify-between flex-1 items-center hover:cursor-pointer"
+          className="flex justify-between flex-1 items-center hover:cursor-pointer gap-[14px]"
           onClick={() => setIsOpen(!isOpen)}
         >
-          <p className="text-body desktop:text-body17">{question}</p>
+          <p className="text-bodySmaller_13_18 desktop:text-body17">
+            {question}
+          </p>
           <button>{isOpen ? <MinusIcon /> : <PlusIcon />}</button>
         </div>
       </div>
       {isOpen && (
-        <div className="border-t border-black border-dashed mt-[35px] pt-[32px]">
-          <p className="text-caption desktop:text-bodySmaller">{answer}</p>
+        <div className="border-t border-black border-dashed mt-[15px] desktop:mt-[35px] pt-[23px] desktop:pt-[32px]">
+          <p className="text-captionMedium_11_20 desktop:text-bodySmaller">
+            {answer}
+          </p>
         </div>
       )}
     </div>
@@ -65,11 +69,11 @@ const FAQPageFAQSection: React.FC = () => {
     },
   ];
   return (
-    <div className="flex flex-col gap-16 desktop:gap-[110px] pb-24 destkop:pb-[50px] relative z-10">
-      <h2 className="text-center text-heading4 desktop:text-heading3 font-medium uppercase tracking-[0.35rem]">
+    <div className="flex flex-col gap-[60px] desktop:gap-[110px] destkop:pb-[50px] relative z-10">
+      <h2 className="text-center text-heading5Larger desktop:text-heading3 font-medium uppercase tracking-[0.35rem]">
         Q & A
       </h2>
-      <div className="w-full max-w-[1008px] mx-auto flex flex-col gap-[20px] px-4 desktop:px-0">
+      <div className="w-full max-w-[1008px] mx-auto flex flex-col gap-[14px] desktop:gap-[20px] px-4 desktop:px-0">
         {faqData.map((faq, index) => (
           <FAQItem
             key={index}
