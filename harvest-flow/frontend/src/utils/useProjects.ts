c86b9c8b3abeafd065ffefc0ce154877c0b75ff4
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-07-31 10:52:22 +0900

    frontend-side whitelist

--- harvest-flow/frontend/src/utils/useProjects.ts
+++ harvest-flow/frontend/src/utils/useProjects.ts
@@ -3,13 +3,27 @@ import type MainController from "@src/MainController";
 import { AppContext } from "@src/main";
 import type { NftContract } from "@harvest-flow/utils";
 
+/**
+ * This whitelist should be in the backend in the future, but for now it's in the frontend
+ */
+const frontendWhitelist = [
+  '0x303117a03305b1c67a62eeef04fb096d9b67af9f'
+];
 export const useProjects = () => {
   const mainController: MainController = useContext(AppContext);
   const [projects, setProjects] = useState<NftContract[]>([]);
 
   useEffect(() => {
     mainController.getAllNft(false).then((projects) => {
-      setProjects(projects);
+      const filteredProjects: NftContract[] = [];
+      for (const project of projects) {
+        if (!frontendWhitelist.includes(project.address)) {
+          console.log(`Filtered NFT ${project.address}`);
+          continue;
+        }
+        filteredProjects.push(project);
+      }
+      setProjects(filteredProjects);
     });
   }, []);
 
