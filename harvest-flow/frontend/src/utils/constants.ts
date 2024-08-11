ce7f2580b808e4867506e7976179128acf70c319
Sebastien Guillemot <sebastiengllmt@gmail.com>
2024-08-11 09:05:35 +0900

    Fix yield calculation in project UI

--- harvest-flow/frontend/src/utils/constants.ts
+++ harvest-flow/frontend/src/utils/constants.ts
@@ -1,6 +1,11 @@
 export const BLOCK_TIME = Number.parseInt(process.env.BLOCK_TIME);
 
-export const YEAR_IN_S = 31556926
+/**
+ * DANGER: This is just an approximation
+ * 31536000000 = 365 * 24 * 60 * 60 * 1000
+ * But this doesn't take into account leap years, etc.
+ */
+export const YEAR_IN_MS = 31536000000 as const;
 
 export const CHAIN_EXPLORER_URI = process.env.CHAIN_EXPLORER_URI;
 
